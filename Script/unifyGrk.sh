38e3c18898076335c4ce222e49f9700347e6d45c
Eliran Wong <hkwebtech@gmail.com>
2018-08-05 23:38:22 +0100

    Add files via upload

---
+++ Script/unifyGrk.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/sed -E -i .bak -f
+#deal with Greek letters
+s/ά/ά/g
+s/ί/ί/g
+s/έ/έ/g
+s/ώ/ώ/g
+s/ή/ή/g
+s/ύ/ύ/g
+s/ό/ό/g
+s/̓͂Α/Ἆ/g
+s/̓͂Η/Ἦ/g
+s/̓͂Ω/Ὦ/g
+s/ί̈/ΐ/g
+s/ΐ/ΐ/g
+s/ΰ/ΰ/g
+s/[᾿ʼ]/᾽/g
+s/ῇ/ῇ/g
